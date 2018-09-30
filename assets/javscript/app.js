4d71b422e8ceb21e9e3a20cec13685f74a197ffb
dean <spencerdberg1@gmail.com>
2018-09-30 16:12:19 -0600

    commit

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -1,21 +1,71 @@
     $(document).ready(function(){
 
-        let topics = ["gymkhana", "Kung Fury", "MotoGP", "top fuel", "Wakesurfing"];
+        $('#rowK').hide();
+        $('.col-md-1').hide();
 
-        
+        function emptyThings(){
 
-            $('#subbtn').click(function(){
+                $('#rowK').show();
+                $('.col-md-1').show();
+                $('#RatingR').empty();
+                $('#gif').empty();
+                $("#moreGif").empty();
+        }
 
-                let mkbtn = $('#mkBtn').val().trim();
+        //array fo buttons
+        let topics = ["Gymkhana", "Kung Fury", "MotoGP", "Top fuel", "Wakesurfing"];
+
+    
+//      click funtion to add a new button
+        $('#subbtn').click(function(event){
+            event.preventDefault();
+            let mkbtn = $('#mkBtn').val().trim();
+         if(mkbtn){
+                topics.push(mkbtn);
+                makeButtons();
+         }
+    
+        });
+
+
+    
+        // function to make ajax query
+        $('#submit').click(function(event){
+        event.preventDefault();
+            emptyThings();
+
+            let search =  $('#term').val().trim();
+                
+            let key = "&api_key=mdrRGguSB0MckrcUeTgQPGBK1c0A4AfP";
+            let url = "http://api.giphy.com/v1/gifs/search?q="+search+key;
+
+            $.ajax({
+                url: url,
+                method: "Get" 
+            }).then(function(response){
+                
+                let data = response.data;
+                console.log(data);
+
+                for(var i = 0; i < 1; i++){
+
+                    $('#gif').append("<img id='giff' src='" +data[i].images.fixed_width.url+ "'/>");
+                    $('#gif').append("<h2 id='RatingT'>"+data[i].rating+"</h2>");
+                }
+
+                for(var i = 2; i < 12; i++){
+
+                $('#moreGif').append("<img id='moreGifs' src='" +data[i].images.fixed_height.url+ "'/>");
+                $('#RatingR').append("<h2 id='Rating"+[i]+"'>"+data[i].rating+"</h2>");
             
-                    topics.push(mkbtn);
-                    makeButtons();
+                }
 
-        
-            });
+            })
+        });
+
        
-        //  function to makeButtons
-        function makeButtons(){
+        //  function to makeButtons for every string in topic array
+            function makeButtons(){
            
             $('#buttons').empty();
             
@@ -24,96 +74,68 @@
                 var input = "<button id='postButton' postButton='"+topics[i]+"' class='btn btn-primary' type='button'>";
                 
                 $('#buttons').append(input+topics[i]+"</button>");
-            };
-              
-        
+            }
+
             $('button').click(function(){
             
-                console.log(this);
 
-
-                $('#RatingR').empty();
-                $('#gif').empty();
-                $("#moreGif").empty();
-
+                emptyThings();
+    
                 var input = $(this).attr("postButton");
-                console.log($(this).attr("postButton"));
                 console.log(input);
                 
                 
                 let key = "&api_key=mdrRGguSB0MckrcUeTgQPGBK1c0A4AfP";
                 let url = "http://api.giphy.com/v1/gifs/search?q="+input+key;
                 
-
-                $.ajax({
-                    url: url,
-                    method: "Get" 
-                }).then(function(response){
-                    
-                    let data = response.data;
-                    console.log(data);
-
-                    for(var i = 0; i < 2; i++){
-
-                        $('#gif').append("<img id='giff' src='" +data[i].images.fixed_height.url+ "'/>");
-                        $('#gif').append("<h2 id='RatingT'>"+data[i].rating+"</h2>");
-                    }
-
-                    for(var i = 2; i < 12; i++){
-
-                        $('#moreGif').append("<img id='moreGifs' src='" +data[i].images.fixed_height.url+ "'/>");
-                        $('#RatingR').append("<h2 id='Rating"+[i]+"'>"+data[i].rating+"</h2>");
-                
-                    }
-
-                })
-            });
-
-
-        
-            // function to make ajax query
-            $('#submit').click(function(){
-            
-                $('#RatingR').empty();
-                $('#gif').empty();
-                $("#moreGif").empty();
-
-                let search =  $('#term').val().trim();
-                if(search == ""){
-                    return;
-                }else{
-                    console.log(search);
-                }
-                    
-                let key = "&api_key=mdrRGguSB0MckrcUeTgQPGBK1c0A4AfP";
-                let url = "http://api.giphy.com/v1/gifs/search?q="+search+key;
-
-                $.ajax({
-                    url: url,
-                    method: "Get" 
-                }).then(function(response){
-                    
-                    let data = response.data;
-                    console.log(data);
-
-                    for(var i = 0; i < 2; i++){
-
-                        $('#gif').append("<img id='giff' src='" +data[i].images.fixed_width.url+ "'/>");
-                        $('#gif').append("<h2 id='RatingT'>"+data[i].rating+"</h2>");
-                    }
-
-                    for(var i = 2; i < 12; i++){
-
-                    $('#moreGif').append("<img id='moreGifs' src='" +data[i].images.fixed_height.url+ "'/>");
-                    $('#RatingR').append("<h2 id='Rating"+[i]+"'>"+data[i].rating+"</h2>");
-                
-                    }
-
-                })
-            });
     
+                $.ajax({
+                    url: url,
+                    method: "Get" 
+                }).then(function(response){
+                    
+                    let data = response.data;
+                    console.log(data);
+
+    
+                    for(var i = 0; i < 1; i++){
+    
+                        var random = Math.floor((Math.random() * 24) + 1);
+                        console.log(random);
+                        $('#gif').append("<img id='giff' gif-status='animate' src='"+data[random].images.fixed_height.url+"' still_Url='"+data[random].images['480w_still'].url+"' animateSrc='" +data[random].images.fixed_height.url+ "'/>");
+                        $('#gif').append("<h2 id='RatingT'>"+data[random].rating+"</h2>");
+                    }
+    
+                    for(var i = 2; i < 12; i++){
+    
+                        var random = Math.floor((Math.random() * 24) + 1);
+                        $('#moreGif').append("<img id='moreGifs' gif-status='animate' src='" +data[random].images.fixed_height.url+ "' still_Url='"+data[random].images['480w_still'].url+"' animateSrc='" +data[random].images.fixed_height.url+"'/>");
+                        $('#RatingR').append("<h2 id='Rating"+[i]+"'>"+data[random].rating+"</h2>");
+                
+                    }
+
+
+                    //click funtion to toggle still image
+                    $("img").on("click", function(){
+                        var state = $(this).attr("gif-status");
+                        var stillUrl = $(this).attr("still_Url");
+                        var urlDisplay = $(this).attr("animateSrc");
+                        console.log(stillUrl);
+
+                        if(state == "animate"){
+                            $(this).attr("gif-status", "still");
+                            $(this).attr("src", stillUrl);
+                        console.log(stillUrl+state);
+                        }else{
+                            $(this).attr("gif-status", "animate");
+                            $(this).attr("src", urlDisplay)
+                        }
+                    })
+                })
+            });
         }
-        
+     
+
 
         makeButtons();
    
